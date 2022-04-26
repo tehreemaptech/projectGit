52ad0354249a5be52946a78c384e1d0a6fdc0ad2
tehreemaptech <tehreemmushtaqaptechnn@gmail.com>
2022-04-26 10:03:46 +0500

    changes to nextfile.php

--- nextfile.php
+++ nextfile.php
@@ -3,5 +3,8 @@
 
 //this is a next file
 
-
+echo "<p>hello, this is next file of my project</p>";
+echo "<br>";
+echo "<hr>";
+echo "<p>The purpose of this file is to provide info about the main agenda of the project</p>";
 ?>
